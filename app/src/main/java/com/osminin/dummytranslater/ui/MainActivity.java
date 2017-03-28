4b75cbc11b7538fa90dbc2c53368a4b770c00d62
osminin <osminin@mera.ru>
2017-03-28 18:29:55 +0300

    update

--- app/src/main/java/com/osminin/dummytranslater/ui/MainActivity.java
+++ app/src/main/java/com/osminin/dummytranslater/ui/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.jakewharton.rxbinding2.view.RxView;
@@ -27,8 +29,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
 
-import static android.R.attr.data;
-
 public class MainActivity extends BaseActivity implements MainView {
     public static final String TRANSLATION_MODEL_KEY = "translation_model_extra";
     private static final int INPUT_TIMEOUT = 300;
@@ -42,10 +42,14 @@ public class MainActivity extends BaseActivity implements MainView {
 
     private CustomAdapter mAdapter;
     private TranslationModel mTranslationModel;
+
+    //card controls
     private View mInputContainer;
     private TextView mInputField;
     private View mTranslationContainer;
     private TextView mTranslationField;
+    private Spinner mFromSpinner;
+    private Spinner mToSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,8 +99,9 @@ public class MainActivity extends BaseActivity implements MainView {
                 makeSceneTransitionAnimation(this, mInputContainer, getString(R.string.main_translate_transition));
         if (mTranslationModel == null) {
             mTranslationModel = new TranslationModel();
-            //TODO:
-            mTranslationModel.setTranslationDirection(Languages.ENGLISH, Languages.RUSSIAN);
+            Languages from = Languages.values()[mFromSpinner.getSelectedItemPosition()];
+            Languages to = Languages.values()[mToSpinner.getSelectedItemPosition()];
+            mTranslationModel.setTranslationDirection(from, to);
         }
         intent.putExtra(TRANSLATION_MODEL_KEY, mTranslationModel);
         startActivityForResult(intent, REQUEST_TRANSLATION_ACTIVITY, options.toBundle());
@@ -108,12 +113,10 @@ public class MainActivity extends BaseActivity implements MainView {
             TranslationModel model = data.getParcelableExtra(TRANSLATION_MODEL_KEY);
             mTranslationModel = model;
             mInputField.setText(model.getPrimaryText());
-            //Translation Card
+            //remove old one Translation Card, update and add updated one
             if (mTranslationContainer != null) {
                 mAdapter.removeTranslationCard(mTranslationContainer);
             }
-            mTranslationContainer = View.inflate(this, R.layout.translation_card_layout, null);
-            mTranslationField = ButterKnife.findById(mTranslationContainer, R.id.translate_result);
             //TODO:
             mTranslationField.setText(mTranslationModel.getTranslations().get(0));
             mAdapter.addTranslationCard(mTranslationContainer);
@@ -132,7 +135,22 @@ public class MainActivity extends BaseActivity implements MainView {
         mRecyclerView.setAdapter(mAdapter);
         //Input card
         mInputContainer = View.inflate(this, R.layout.input_card_layout, null);
-        mInputField = ButterKnife.findById(mInputContainer, R.id.main_edit_text);
+        mInputField = ButterKnife.findById(mInputContainer, R.id.input_edit_text);
         mAdapter.addInputCard(mInputContainer);
+        //Translation Card init
+        mTranslationContainer = View.inflate(this, R.layout.translation_card_layout, null);
+        mTranslationField = ButterKnife.findById(mTranslationContainer, R.id.translate_result);
+        initSpinners();
+    }
+
+    private void initSpinners() {
+        mFromSpinner = ButterKnife.findById(mInputContainer, R.id.input_from_spinner);
+        mToSpinner = ButterKnife.findById(mInputContainer, R.id.input_to_spinner);
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, Languages.getHRStrings(this));
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mFromSpinner.setAdapter(spinnerArrayAdapter);
+        mToSpinner.setAdapter(spinnerArrayAdapter);
     }
 }
